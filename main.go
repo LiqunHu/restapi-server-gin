6d28e9c49eded8bed10b2dd3fa5debd2a7e1e996
胡力群 <huliquns@126.com>
2022-01-24 16:49:55 +0800

    增加优雅退出

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/LiqunHu/restapi-server-gin/models"
 	"github.com/LiqunHu/restapi-server-gin/pkg/gredis"
@@ -39,5 +43,23 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			logger.Infof("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	logger.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		logger.Fatalf("Server Shutdown: %s\n", err)
+	}
+	logger.Info("Server exiting")
+	// s.ListenAndServe()
 }
