b3dfde46e5a560ba02f0d6450ffa54be7cae4f37
胡力群 <huliquns@126.com>
2022-06-10 17:49:05 +0800

    优化登录

--- service/v1/auth/model.go
+++ service/v1/auth/model.go
@@ -1,8 +1,7 @@
 package auth
 
 type SigninIN struct {
-	LoginType    string `json:"LoginType" binding:"required,oneof=WEB MOBILE"`
-	Username     string `json:"Username" binding:"required,max=100"`
-	IdentifyCode string `json:"IdentifyCode" binding:"required,max=100"`
-	MagicNo      string `json:"MagicNo" binding:"required,max=100"`
+	LoginType    string `json:"login_type" binding:"required,oneof=ADMIN WEB MOBILE"`
+	Username     string `json:"username" binding:"required,max=100"`
+	IdentifyCode string `json:"identify_code" binding:"required,max=100"`
 }
